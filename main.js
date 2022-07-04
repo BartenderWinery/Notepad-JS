d1376b49e8d6760658a76cb991dd4d96984f595f
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2022-07-04 12:40:25 -0400

    saving almost in place

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const{app,BrowserWindow,ipcMain}=require("electron")
 require("electron-reload")(__dirname, {
     electron:require(`${__dirname}/node_modules/electron`)})
 const path=require("path")
+const fs=require("fs")
+//Important. Clean up imports so it doesn't slow down loading times.
 
 var win=[]
 function popout(bounds,page){
@@ -20,18 +22,24 @@ function popout(bounds,page){
         win[win.length-1].loadFile(path.join(__dirname,page))
         setTimeout(function(){win[win.length-1].show()}),500}
 async function main(){popout([580,300],"index.html")}
-var temp={
-    path:function(){}}
 app.on("ready",main)
 ipcMain.on("minimize",()=>{BrowserWindow.getFocusedWindow().minimize()})
 ipcMain.on("maximize",()=>{BrowserWindow.getFocusedWindow().maximize()})
 ipcMain.on("close",()=>{BrowserWindow.getFocusedWindow().close()})
 ipcMain.on("popout",(events,args)=>{popout([400,220],args)})
 ipcMain.on("save",(events,args)=>{
-    console.group("Saving")
-        console.info("Saving in progress...")            
-    if(!temp.path(args["name"]))popout([580,300],"resources/folders.html")
-    else{
-        save(temp.path(args["name"]))
-        console.info("Saving completed.")}})
+    win=BrowserWindow.getAllWindows().at(-1).webContents
+    win.executeJavaScript("console.group('Saving')")
+    win.executeJavaScript("console.info('Saving in progress...')")         
+    var p=path.join(__dirname,"temp/text.txt")
+    if(args) 
+        fs.writeFile(p,args,(e)=>{
+            if(e)return})
+    else
+        fs.readFile(p,(e,data)=>{
+            if(e)return})
+    //if(!temp.path(args["name"]))popout([580,300],"resources/folders.html")
+    //else{
+    win.executeJavaScript("console.info('Saving completed.')")
+    win.executeJavaScript("console.groupEnd()")})
 ipcMain.on("load",()=>{})
--- resources/preload.js
+++ resources/preload.js
@@ -8,8 +8,4 @@ contextBridge.exposeInMainWorld(
         send: (channel, data) => {
             let validChannels = "minimize;maximize;close;popout;save";
             if (validChannels.includes(channel)) {
-                ipcRenderer.send(channel, data)}},
-        receive: (channel, func) => {
-            let validChannels = "minimize;maximize;close;popout;save";
-            if (validChannels.includes(channel)) {
-                ipcRenderer.on(channel, (event, ...args) => func(...args))}}})
+                ipcRenderer.send(channel, data)}}})
